bc13db871b2dd7efa6657a064810f5a5353ecc5c
tim <timur.nasibullin@gmail.com>
2014-04-01 02:03:22 +0400

    removed submit button

--- src/main/webapp/js/valet.js
+++ src/main/webapp/js/valet.js
@@ -1,3 +1,10 @@
 (function($, valet) {
+    var KBD_ENTER = 13;
 
+    var addNoteForm = $("#addNoteForm");
+    addNoteForm.keydown(function (event) {
+        if (event.keyCode == KBD_ENTER) {
+            addNoteForm.submit();
+        }
+    });
 }) (jQuery, window.valet = window.valet || {})
